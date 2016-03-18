0648126d8d0eb77f14f88ed87f41fab074e8dff1
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-03-18 16:28:07 -0400

    changed it so it sets the input to 0

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ var $list = $('.list');
 
 $('form').on('submit', function (e) {
   var $li = $('<li>');
+
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+  $input.val('');
 });
 
 $('ul').on('click', 'li', function (e) {
